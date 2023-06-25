0e08156a78a574e35cd02734aba293006519c28e
Miyanber <miyanba2984h@gmail.com>
2023-06-25 15:21:33 +0900

    06/25

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-fetch("header.html", {
+const options = {
     method: 'GET',
     headers: {
-      'Content-Type': 'text/html',
+        'Content-Type': 'text/plain',
     },
-}).then((content) => {
-    document.body.innerHTML = content.text() + document.body.innerHTML;
-})
+}
+
+function loadHeader() {
+    fetch("header.html", options)
+        .then((content) => content.text()
+        ).then((html) => {
+            const header = document.createElement("header");
+            document.body.insertBefore(header, document.body.firstChild);
+            header.outerHTML = html;
+            header.classList.add("subPage");
+            console.log("loaded header")
+
+            // DOMParserでHTMLを読み込める!
+            // const parser = new DOMParser();
+            // return parser.parseFromString(html, "text/html");
+        })
+}
+
+function loadFooter() {
+    fetch("footer.html", options)
+        .then((content) => {
+            return content.text()
+        }).then((html) => {
+            const footer = document.createElement("footer");
+            document.body.insertBefore(footer, document.body.lastChild);
+            footer.outerHTML = html;
+            footer.classList.add("subPage");
+            console.log("loaded footer")
+        })
+}
